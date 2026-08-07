fee59ca222c9c370576a2b905789ac13ead8ddc1
MehmoodAhmed786 <joblessveelawger786@gmail.com>
2026-08-07 05:23:37 +0500

    swagger added

--- obj/Debug/net10.0/curd_api.AssemblyInfo.cs
+++ obj/Debug/net10.0/curd_api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("curd_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02dec5eed4c13444a60267ebafc5841f645b4de9")]
 [assembly: System.Reflection.AssemblyProductAttribute("curd_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("curd_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
